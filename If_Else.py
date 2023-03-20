0239b5abcb9b0e8c52c010d9c085da4e2c887464
Aung_Myint <aungmyint3112@gmail.com>
2023-03-20 16:13:22 +0630

    adding condition and lambda function

---
+++ If_Else.py
@@ -0,0 +1,26 @@
+x=100
+y=200
+z=300
+if(x==z):
+ print('x equal to z')
+if x != y:
+    print("x not equal to y")
+if (x<y):
+    print("x  is less than y")
+if x <= z :
+    print("x is less than or equal to z")
+if y > x:
+    print ('y is greater than x')
+if y >= x:
+    print ('y is greater than or equal to x')
+if x <y :
+    print ("x is less than y")
+elif x > y :
+    print('x is greater than y')
+else:
+    print ('x is equal to y')
+if x<y : print('x is less than y')
+if (x < y and z < y):
+    print("x is less than y and  z is less than y")
+if (x < y or z < y):
+    print("x is less than y and  z is less than y")
---
+++ Lambda.py
@@ -0,0 +1,4 @@
+add = lambda x,y : x+y
+print (add(3,4)) 
+multiply = lambda x,y,z : x* y* z
+print(multiply(2,2,2,))
